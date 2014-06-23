b3bf64ec244a45ada617f0648f22a436242496a1
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-06-23 16:35:03 -0700

    Added in license info

--- js/kraken.js
+++ js/kraken.js
@@ -1,3 +1,23 @@
 /* =============================================================
     Add your scripts here.
  * ============================================================= */
+
+//==========================================================================//
+// This file is part of openlysecular.                                      //
+//                                                                          //
+// openlysecular is Copyright 2014 Volary Foundation and Contributors       //
+//                                                                          //
+// openlysecular is free software: you can redistribute it and/or modify it //
+// under the terms of the GNU Affero General Public License as published    //
+// by the Free Software Foundation, either version 3 of the License, or     //
+// at your option) any later version.                                       //
+//                                                                          //
+// openlysecular is distributed in the hope that it will be useful, but     //
+// WITHOUT ANY WARRANTY; without even the implied warranty of               //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        //
+// Affero General Public License for more details.                          //
+//                                                                          //
+// You should have received a copy of the GNU Affero General Public         //
+// License along with openlysecular.  If not, see                           //
+// <http://www.gnu.org/licenses/>.                                          //
+//==========================================================================//
